3b1201e0eede0773921303faeb674b61912fbbdf
wexblack <wexblack@gmail.com>
2015-11-26 21:37:57 +0100

    Initial Commit

---
+++ TopSonic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TopSonic.MvcApplication" Language="C#" %>
---
+++ TopSonic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TopSonic.Startup))]
+namespace TopSonic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
